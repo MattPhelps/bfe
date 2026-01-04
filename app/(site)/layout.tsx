ca7b62da94f111d0244b132f24a731873bd64ecd
Matt Phelps <matt@leandme.com>
2026-01-04 22:15:38 +0100

    added the adsense shit

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import AmplitudeInitializer from "../components/AmplitudeInitializer";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import Script from "next/script";
 
 export default function RootLayout({
   children,
@@ -12,6 +13,10 @@ export default function RootLayout({
     <html lang="en">
       <body className="bg-base-100 text-base-content">
       <AmplitudeInitializer />
+      <Script>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9184203217182921"
+     crossOrigin="anonymous"></script>
+      </Script>
         <Navbar />
         <main className="container mx-auto px-4 lg:px-8 py-8 min-h-screen">
           {children}
