ed6e1db57d388581d11ddb05d11dab9aba3c499d
Matt Phelps <matt@leandme.com>
2026-01-04 22:37:36 +0100

    ok

--- app/components/AmplitudeInitializer.tsx
+++ app/components/AmplitudeInitializer.tsx
@@ -8,14 +8,14 @@ const AmplitudeInitializer = () => {
   useEffect(() => {
     if (AMPLITUDE_API_KEY) {
       try {
-        amplitude.init(AMPLITUDE_API_KEY, {
-          autocapture: {
-            formInteractions: false,
-            fileDownloads: false,
-            elementInteractions: false,
-          },
-          serverZone: "EU"
-        });
+        amplitude.init(AMPLITUDE_API_KEY, undefined, {
+  autocapture: {
+    formInteractions: false,
+    fileDownloads: false,
+    elementInteractions: false,
+  },
+  serverZone: "EU",
+});
         console.log("Amplitude initialized successfully.");
       } catch (err) {
         console.error("Error initializing Amplitude:", err);
