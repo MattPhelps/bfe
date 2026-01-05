06c34646a04e674a8eeb7ee7c484b7fe9f93e47f
Matt Phelps <matt@leandme.com>
2026-01-05 22:30:14 +0100

    ok

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,24 +2,23 @@ export default function Footer() {
     return (
         <footer className="footer bg-[#e3e3e3] text-base-content p-10">
             <aside>
-            <img src="/favicon.ico" alt="Body Fat Estimator Logo" className="w-8 h-8 "/>
                 <p className="text-base">
-                    <b className="font-heading">Body Fat Estimator</b>
+                    <b className="font-heading text-lg">Body Fat Estimator</b>
                     <br />
-                    Copyright 2026 - all rights reserved.
+                    <span className="text-sm">Copyright 2026 - all rights reserved.</span>
                 </p>
 
             </aside>
             <nav>
-                <h4 className="footer-title font-heading text-lg">Support</h4>
                 <p><a className="link link-hover" href="/about">About</a></p>
                 <p><a className="link link-hover" href="/#faqs">FAQs</a></p>
                 <p><a className="link link-hover" href="/contact">Contact</a></p>
             </nav>
             <nav>
-                <h4 className="footer-title font-heading text-lg">Legal</h4>
-                <p><a className="link link-hover" href="/terms">Terms of use</a></p>
-                <p><a className="link link-hover" href="/privacy">Privacy policy</a></p>
+             {/*   <p><a className="link link-hover" href="/blog">Blog</a></p> */} 
+                <p><a className="link link-hover" href="/terms">Terms</a></p>
+                <p><a className="link link-hover" href="/privacy">Privacy</a></p>
+                <p><a className="link link-hover" href="/cookies">Cookies</a></p>
             </nav>
         </footer>
     );
