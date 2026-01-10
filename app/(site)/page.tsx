751768204d6b9d0c8fe34f38b746a0634bfe1a3f
Matt Phelps <matt@leandme.com>
2026-01-10 17:09:31 +0100

    fuck you seo

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -7,7 +7,7 @@ import Comparison from "../components/Comparison";
 import HeroWithUpload from "../components/HeroWithUpload";
 
 const title = "Body Fat Estimator – What Body Fat % Are You?";
-const description = "Upload a photo and let our AI calculate your body fat percentage. Get a quick and accurate body fat % estimation to track your progress.";
+const description = "Upload a photo and let our AI calculate your body fat percentage. Get a quick and accurate body fat percentage estimation to track your progress.";
 
 export const metadata: Metadata = {
   title: title,
